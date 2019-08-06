96f50136cb1c3562d3cfd3bd688115771a6517ff
PedrobjjAlmeida <53578121+PedrobjjAlmeida@users.noreply.github.com>
2019-08-06 16:49:23 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,4 +4,25 @@ LS-LH: lista de conteúdo Do Diretório
 CTRL-L: Limpa tudo
 ~ : Diretório pessoal
 CD/: raiz Do PC
-Azul Escuro:
+Azul Escuro: Significa Diretório
+Azul Claro: Atalhos
+Verde: Perigo
+Branco: Arquivo
+PWD: É onde eu estou,localização
+MKDIR: Nome criar Diretório
+TAB: Auto Completa
+SUDO: Super Usuário
+Echo: Repetição Do que se escreve
+Git Clano: Caminho Do projeto,clonar arquivos feito no git
+Sudo Apt Update && Sudo Install Git: Baixar o git para ter interação com o GITHUB
+Touch: Crair arquivos,mas senão estiver criado ele muda data e hora
+Cat:Para lista de conteúdos de arquivos escolhidos
+Cat -n: Contra linha também
+Head: Lista o inicio Do arquivo,numa tradução
+Head -20: Mostra até a linha indicada
+Tail:mostra o rodapé de baixo para cima
+Tail: -20n: mostra as 20 ultimas linhas
+Less: ler o conteudo Do arquivo como se fosse um leitor
+"Q" sair
+"|" filtrar
+Cat -n LICENSE|tail: SAir de um arquivo para  entrar em outro
