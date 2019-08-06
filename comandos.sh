965333cfd9af0cf499b630d399bd5f136e56b9a6
PedrobjjAlmeida <53578121+PedrobjjAlmeida@users.noreply.github.com>
2019-08-06 17:12:33 -0300

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,59 @@
+    1  ls
+    2  ls -lh
+    3  cd /
+    4  ls
+    5  cd -
+    6  pwd
+    7  mkdir linuxmint
+    8  ls
+    9  mkdir linuxmint
+   10  ls
+   11  cd linuxmint/
+   12  ls
+   13  sudo
+   14  sudo apt  update  && S
+   15  sudo apt update && sudo apt install git
+   16  git clone
+   17  git clone https://github.com/PedrobjjAlmeida/linuxmint.git
+   18  ls
+   19  cd linuxmint
+   20  ls-lh
+   21  echo SENAC Tatuapé aprendendo Bash Shell  no Linux Mint
+   22  echo SENAC Tatuapé aprendendo Bash Shell  no Linux Mint > Pedro.sh
+   23  ls
+   24  ls-lh
+   25  ls -lh
+   26  touch senac.sh
+   27  ls
+   28  ls -lh
+   29  cat L
+   30  cat LICENSE 
+   31  cat -n  LICENSE 
+   32  head LICENSE 
+   33  head -n20 LICENSE 
+   34  ls
+   35  ls -lh
+   36  cd L
+   37  cd linux mint
+   38  cd linux-mint/
+   39  tail LICENSE 
+   40  tail -20n LICENSE 
+   41  tail -20n
+   42  less LICENSE 
+   43  cat -n LICENSE | tail
+   44  cat -n LICENSE | tail -n100
+   45  cat -n100 LICENSE | tail
+   46  cat -n LICENSE | tail -n100
+   47  head -n100 LICENSE 
+   48  ls
+   49  git pull
+   50  cat -n LICENSE | tail -n20 | head-100
+   51  cat -n LICENSE | tail -n20 | head -200
+   52  cat -n LICENSE | tail -n20 | head -100
+   53  cat -n LICENSE | tail -n20 | head -101
+   54  cat -n LICENSE | tail -n200 | head -n101
+   55  cat -n LICENSE | head-n200
+   56  cat -n LICENSE | head -n200
+   57  cat -n LICENSE | head -n200 | tail -n101
+   58  history
+   59  history > comandos.sh
